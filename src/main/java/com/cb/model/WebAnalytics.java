8175e18000e679b93724ab9aafd5da3aef72dd48
nkchauhan003 <nkchauhan003@gmail.com>
2023-02-10 21:54:05 +0530

    added rest endpoints

--- src/main/java/com/cb/model/WebAnalytics.java
+++ src/main/java/com/cb/model/WebAnalytics.java
@@ -1,17 +1,11 @@
 package com.cb.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
-
-import java.util.Date;
 
 @Table("web_analytics")
 @AllArgsConstructor
--- src/main/java/com/cb/repository/WebAnalyticsRepository.java
+++ src/main/java/com/cb/repository/WebAnalyticsRepository.java
@@ -3,13 +3,6 @@ package com.cb.repository;
 import com.cb.model.WebAnalytics;
 import com.cb.model.WebAnalyticsPrimaryKey;
 import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.data.cassandra.repository.Query;
-
-import java.util.List;
 
 public interface WebAnalyticsRepository extends CassandraRepository<WebAnalytics, WebAnalyticsPrimaryKey> {
-
-    @Query("SELECT * FROM web_analytics WHERE date IN ('2023-02-10 00:00:00.000000+0000') and site_id=10")
-    List<WebAnalytics> getByDateRangeAndSiteId();
-
 }
